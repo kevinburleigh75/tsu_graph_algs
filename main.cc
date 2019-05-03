65ca6bbb2d4e5bd64803d5f910d4643fa5e0bb1c
Kevin Burleigh <klb@kindling>
2019-05-02 23:12:01 -0500

    adjust output of connected components

--- main.cc
+++ main.cc
@@ -46,6 +46,24 @@ int main ()
   dg.add_edge({9,10});
   dg.add_edge({9,11});
 
+  Graph cg(9, true);
+
+  cg.add_edge({0,1});
+  cg.add_edge({1,2});
+  cg.add_edge({1,4});
+  cg.add_edge({2,4});
+  cg.add_edge({2,5});
+  cg.add_edge({3,0});
+  cg.add_edge({3,6});
+  cg.add_edge({4,3});
+  cg.add_edge({4,5});
+  cg.add_edge({4,7});
+  cg.add_edge({5,8});
+  cg.add_edge({6,7});
+  cg.add_edge({7,6});
+  cg.add_edge({7,8});
+  cg.add_edge({8,7});
+
   if (false) {
     Bfs::NodeCallback ncb_1 = [](const Node& node) {
       cout << "node " << node << " cb 1" << endl;
@@ -66,8 +84,8 @@ int main ()
 
   if (false) {
     auto components = ConnectedComponents::process(ug);
-    for (auto& component : components) {
-      cout << component << endl;
+    for (auto& node : ug.nodes()) {
+      cout << node << " " << components[node] << endl;
     }
   }
 
@@ -121,4 +139,12 @@ int main ()
     }
     cout << endl;
   }
+
+  if (false) {
+    auto components = StrongComponents::process(cg);
+    for (auto& node : ug.nodes()) {
+      cout << node << " " << components[node] << endl;
+    }
+  }
+
 }
